3c5b92d66b3d8f000acebb3bba99ccdd8d85c6b3
Lochrine <37122774+Lochrine@users.noreply.github.com>
2018-04-09 22:02:27 -0400

    Add files via upload

---
+++ First.java
@@ -0,0 +1,16 @@
+/* *****************
+This is a comment!
+Written by Michael McLaughlin
+9/9/14 v1
+This program outputs Hello World! to output.
+This is my first program.  Awwww yeahhh!
+****************** */
+
+public class First {
+   public static void main(String[] args) {
+      //The next line is where I output information.
+      // \t creates a tab.  \n creates a new line.  I'm learning stuff!!!
+      System.out.println("\"Hello World!\"\t\"Another Quotation!\"\n\n");
+      
+   }
+}
